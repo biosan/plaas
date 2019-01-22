7666dd07834f778a84072587cee550e8e2e8cddf
biosan <alessandro@biondi.me>
2019-01-22 13:12:04 +0100

    Add configurable logging level
    Use LOG_LEVEL env variable to set logging level
    
    Use the same level names of logging lib (i.e. DEBUG, CRITICAL, etc.)
    
    Default to INFO

--- plaas/app.py
+++ plaas/app.py
@@ -3,6 +3,9 @@ import falcon
 import logging, logging.config
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger()
+# Hacky way to set logging level from environ variable. default: INFO
+from os import environ
+logger.setLevel(logging._nameToLevel.get(environ.get("LOG_LEVEL", "INFO"), "INFO"))
 
 from .plot import Plot
 
